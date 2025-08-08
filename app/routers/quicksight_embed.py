1541e45da698cc47f0264271c0d9429896173cab
Bones <learngitfrombones@gmail.com>
2025-08-08 11:23:32 +0530

    embedded dash board url

--- app/routers/quicksight_embed.py
+++ app/routers/quicksight_embed.py
@@ -91,6 +91,12 @@ def generate_dashboard_embed_url(req: DashboardEmbedRequest, request: Request):
             UserArn=req.userarn,
             ExperienceConfiguration={"Dashboard": {"InitialDashboardId": req.dashboardid}},
             SessionLifetimeInMinutes=600,
+
+            AllowedDomains=[
+            "http://localhost:4200",
+            "http://54.234.194.203"
+           ]
+           
         )
         logger.info("Embed URL generated | expiresAt=%s", resp.get("Expiration"))
         return {"embedUrl": resp["EmbedUrl"], "expiresAt": resp.get("Expiration")}
